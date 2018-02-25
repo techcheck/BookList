498c45c856677ac68638251c7ce78a3683e55d61
hinge <hinge15220@gmail.com>
2018-02-24 18:07:32 -0600

    Change for Docker Compose Added functionality to check Docker persistent Volume and Load Balancing in Compose Service

--- src/main/java/com/demo/web/app01/controller/BookController.java
+++ src/main/java/com/demo/web/app01/controller/BookController.java
@@ -2,10 +2,14 @@ package com.demo.web.app01.controller;
 
 import com.demo.web.app01.model.Book;
 import com.demo.web.app01.service.BookService;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class BookController {
@@ -31,6 +35,20 @@ public class BookController {
     @PostMapping("/add")
     public List<Book> addBook(@RequestBody Book newBook) {
         return bookService.addBook(newBook);
+    }
+
+    @GetMapping("/hostInfo")
+    public JSONObject getHostPodInfo() throws ParseException {
+        Map<String, String> env =  System.getenv();
+        JSONObject object = (JSONObject) new org.json.simple.parser.JSONParser().parse(JSONObject.toJSONString(env));
+        return object;
+    }
+
+    @GetMapping("/createLogs")
+    public String generateLogs(){
+        String generatedString = RandomStringUtils.randomAlphabetic(50);
+        System.out.println(generatedString);
+        return "Logs Created";
     }
 
 }
